8155f6d4ee48952aa756a102fca9c873bc448a1d
guruvenkatakrishna <VENKATCHALLA841@GMAIL.COM>
2024-11-10 21:04:16 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,27 +3,30 @@ node('agent1') {
   def tomcatUser = 'ubuntu'
   def tomcatHost = '172.31.9.248'
   def tomcatPath = '/home/ubuntu/tomcat/webapps/'
+  
   stage('Checkout') {
-// Checkout source code from Git
+    // Checkout source code from Git
     checkout([$class: 'GitSCM',
-          branches: [[name: '*/main']],  
-          userRemoteConfigs: [[
-            url: 'https://github.com/guruvenkatakrishna/java-example.git'
-          ]]
-        ])
+              branches: [[name: '*/main']],  // Correct format for branch name
+              userRemoteConfigs: [[
+                url: 'https://github.com/guruvenkatakrishna/java-example.git'
+              ]]
+            ])
+  }
 
-  }	
   stage('Build') {
-   // Set Maven tool
-     mvnHome = tool 'Maven'
+    // Set Maven tool
+    mvnHome = tool 'Maven'
+    
     // Run Maven build
-     sh "'${mvnHome}/bin/mvn' clean package"
-// Archive the build artifacts
-     archiveArtifacts artifacts: '**/target/*.war'
-   }
-   stage('Deploy') {
-       
-      sh "sudo cp /home/ubuntu/jenkins/workspace/jenkinsfile.groovy/target/*.war ${tomcatPath}"
-   }
-}
+    sh "'${mvnHome}/bin/mvn' clean package"
+    
+    // Archive the build artifacts
+    archiveArtifacts artifacts: '**/target/*.war'
+  }
 
+  stage('Deploy') {
+    // Copy the WAR file to the Tomcat webapps directory
+    sh "sudo cp ${WORKSPACE}/target/*.war ${tomcatPath}"
+  }
+}
